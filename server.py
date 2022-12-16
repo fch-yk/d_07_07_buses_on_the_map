d4dd41023ef8adfc4f3d72de5487a7bb0551fb5c
yk <yefim_korshever@mail.ru>
2022-12-16 15:50:12 +0300

    Display two buses

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
+import json
 
 
 async def echo_server(request):
     ws = await request.accept()
     while True:
         try:
-            message = await ws.get_message()
-            await ws.send_message(message)
+            input_message = await ws.get_message()
+            print(input_message)
+            output_message = {
+                "msgType": "Buses",
+                "buses": [
+                    {"busId": "c790сс", "lat": 55.7500,
+                        "lng": 37.600, "route": "120"},
+                    {"busId": "a134aa", "lat": 55.7494,
+                        "lng": 37.621, "route": "670к"},
+                ]
+            }
+            output_message = json.dumps(output_message)
+            await ws.send_message(output_message)
         except ConnectionClosed:
             break
 
