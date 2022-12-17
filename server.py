5c2afa4463fd52c9a35cfe62703df30ac3ec82f0
yk <yefim_korshever@mail.ru>
2022-12-17 10:10:22 +0300

    Move one bus

--- server.py
+++ server.py
@@ -1,25 +1,38 @@
-import trio
-from trio_websocket import serve_websocket, ConnectionClosed
 import json
+
+import trio
+from trio_websocket import ConnectionClosed, serve_websocket
 
 
 async def echo_server(request):
     ws = await request.accept()
+    route_path = 'routes/156.json'
+    route = ''
+    async with await trio.open_file(
+        route_path,
+        mode='r',
+        encoding='UTF-8'
+    ) as file:
+        async for line in file:
+            route += line
+    route = json.loads(route)
+    route_name = route['name']
+    bus_id = 'c790сс'
     while True:
         try:
-            input_message = await ws.get_message()
-            print(input_message)
-            output_message = {
-                "msgType": "Buses",
-                "buses": [
-                    {"busId": "c790сс", "lat": 55.7500,
-                        "lng": 37.600, "route": "120"},
-                    {"busId": "a134aa", "lat": 55.7494,
-                        "lng": 37.621, "route": "670к"},
-                ]
-            }
-            output_message = json.dumps(output_message)
-            await ws.send_message(output_message)
+            for latitude, longitude in route['coordinates']:
+                output_message = {
+                    'msgType': 'Buses',
+                    'buses': [
+                        {
+                            'busId': bus_id, 'lat': latitude,
+                            'lng': longitude, 'route': route_name,
+                        },
+                    ]
+                }
+                output_message = json.dumps(output_message)
+                await ws.send_message(output_message)
+                await trio.sleep(1)
         except ConnectionClosed:
             break
 
@@ -27,4 +40,5 @@ async def echo_server(request):
 async def main():
     await serve_websocket(echo_server, '127.0.0.1', 8000, ssl_context=None)
 
-trio.run(main)
+if __name__ == '__main__':
+    trio.run(main)
