cfbe501ad0e0acb6c3cd13aff61ba61d77b141e4
yk <yefim_korshever@mail.ru>
2022-12-19 10:35:05 +0300

    Run some buses per each route

--- fake_bus.py
+++ fake_bus.py
@@ -1,6 +1,8 @@
 import json
 import os
+import random
 import sys
+import itertools
 
 import trio
 from trio_websocket import open_websocket_url
@@ -14,15 +16,19 @@ def load_routes(directory_path='routes'):
                 yield json.load(file)
 
 
-async def run_bus(url, bus_id, route):
+def generate_bus_id(route_id, bus_index):
+    return f"{route_id}-{bus_index}"
+
+
+async def run_bus(url, bus_id, route_name, coordinates):
     async with open_websocket_url(url) as ws:
         while True:
-            for latitude, longitude in route['coordinates']:
+            for latitude, longitude in coordinates:
                 output_message = {
                     'busId': bus_id,
                     'lat': latitude,
                     'lng': longitude,
-                    'route': bus_id
+                    'route': route_name
                 }
                 output_message = json.dumps(output_message, ensure_ascii=False)
 
@@ -33,10 +39,31 @@ async def run_bus(url, bus_id, route):
 async def main():
     directory_path = 'routes'
     url = 'ws://127.0.0.1:8080/'
+    min_per_route = 3
+    max_per_route = 10
     try:
         async with trio.open_nursery() as nursery:
             for route in load_routes(directory_path):
-                nursery.start_soon(run_bus, url, route['name'], route)
+                basic_coordinates = route['coordinates']
+                per_route = random.randint(min_per_route, max_per_route)
+                for bus_index in range(per_route):
+                    bus_id = generate_bus_id(route['name'], bus_index)
+                    start_location = random.randint(
+                        0,
+                        len(basic_coordinates) - 1
+                    )
+                    coordinates = itertools.cycle(
+                        basic_coordinates[start_location::] +
+                        basic_coordinates[:start_location]
+                    )
+
+                    nursery.start_soon(
+                        run_bus,
+                        url,
+                        bus_id,
+                        route['name'],
+                        coordinates
+                    )
     except OSError as ose:
         print(f'Connection attempt failed: {ose}', file=sys.stderr)
 
